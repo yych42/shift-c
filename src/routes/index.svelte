a14580b55177bf7d29fad46662a99f7a588e242f
Yaoyu Chen <yaoyuchen@eml.cc>
2022-01-10 16:15:14 -0800

    Implement homepage

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,60 @@
 <script>
-	let count = 0;
+	import { goto } from '$app/navigation';
+
+	import { onMount } from 'svelte';
+	import { fade, fly } from 'svelte/transition';
+
+	let shouldShow = false;
+
+	onMount(() => {
+		shouldShow = true;
+	});
+
+	function start() {
+		shouldShow = false;
+		const progress = {
+			step: '/start',
+			finished: false,
+			data: {}
+		};
+		// Write progress to local storage
+		localStorage.setItem('progress', JSON.stringify(progress));
+		// Wait for 400 ms for the animation to finish
+		setTimeout(() => {
+			goto('/start');
+		}, 400);
+	}
 </script>
 
-<div class="p-12">
-	<h1>Welcome to SvelteKit</h1>
-	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-	<button
-		class="text-xl mt-2 bg-svelte-prime-900 text-black py-2 px-6 rounded border border-gray-300 shadow-sm"
-		on:click={() => {
-			count = count + 1;
-		}}
+{#if shouldShow}
+	<h1
+		class="text-8xl font-semibold text-indigo-800"
+		in:fade={{ duration: 1000, delay: 400 }}
+		out:fade={{ duration: 400 }}
 	>
-		Clicks: {count}
-	</button>
-</div>
+		Hey, night owl.
+	</h1>
+	<h1
+		class="mt-4 text-8xl font-semibold text-indigo-800"
+		in:fade={{ duration: 1000, delay: 1400 }}
+		out:fade={{ duration: 400 }}
+	>
+		Ready for a change?
+	</h1>
+	<p
+		class="mt-32 max-w-3xl text-center"
+		in:fade={{ duration: 1000, delay: 2400 }}
+		out:fade={{ duration: 400 }}
+	>
+		There's an evidence-based way to shift your circadian rhythm, so you can go to sleep and wake up
+		earlier without the struggle.
+	</p>
+	<div class="mt-8" in:fly={{ y: 8, duration: 1000, delay: 3400 }} out:fade={{ duration: 400 }}>
+		<button
+			on:click={start}
+			class="items-center px-6 py-3 border border-transparent text-base font-medium rounded-full shadow-sm text-white bg-indigo-800 hover:bg-indigo-900 transition-colors"
+		>
+			Get started
+		</button>
+	</div>
+{/if}
